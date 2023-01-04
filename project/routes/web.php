f995b653fc26a9a85d60eda63636930d6fbc1740
Abdullah <abdullahwaseem.4401@gmail.com>
2023-01-04 18:29:44 +0500

    hm

--- project/routes/web.php
+++ project/routes/web.php
@@ -11,6 +11,10 @@ Route::get('/clear', function(){
     return '<h1>All Cache cleared</h1>';
 });
 
+Route::get('sforget', function ($id) {
+    
+});
+
 // use Illuminate\Routing\Route;
 // use Illuminate\Support\Facades\Auth;
 
