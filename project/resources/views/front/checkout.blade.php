132b15b7581308f42a071b805b8b5df51f422d5e
Abdullah <abdullahwaseem.4401@gmail.com>
2023-01-25 19:29:16 +0500

    done for sec

--- project/resources/views/front/checkout.blade.php
+++ project/resources/views/front/checkout.blade.php
@@ -361,6 +361,54 @@
 </style>
 @endsection
 @section('scripts')
+@php
+ $coupon = Session::has('coupon') ? Session::get('coupon') : '0.00';   
+@endphp
+<script>
+   let productArr = <?php echo json_encode($products); ?>;
+   console.log(productArr);
+   var items = [{
+      id: productArr.item
+   }];
+   console.log(items);
+</script>
+<script>
+
+   gtag("event", "purchase", {
+       transaction_id: "T_12345_1",
+       value: 25.42,
+       tax: {!!$gs->tax!!},
+       currency: "USD",
+       coupon: {!!$coupon!!},
+       items: [
+       // If someone purchases more than one item, 
+       // you can add those items to the items array
+        {
+         item_id: "SKU_12345",
+         item_name: "Stan and Friends Tee",
+         affiliation: "Google Merchandise Store",
+         coupon: "SUMMER_FUN",
+         discount: 2.22,
+         index: 0,
+         item_brand: "Google",
+         item_category: "Apparel",
+         item_category2: "Adult",
+         item_category3: "Shirts",
+         item_category4: "Crew",
+         item_category5: "Short sleeve",
+         item_list_id: "related_products",
+         item_list_name: "Related Products",
+         item_variant: "green",
+         location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
+         price: 9.99,
+         quantity: 1
+       }]
+   });
+   </script>
+
+
+
+
 <script type="text/javascript">
    var mainurl = "{{url('/')}}";
    var gs      = {!! json_encode($gs) !!};
