1280149a64499cb2796a4d5e3bac82fa15a9546d
Abdullah <abdullahwaseem.4401@gmail.com>
2023-01-25 21:09:19 +0500

    checkpiu

--- project/resources/views/front/checkout.blade.php
+++ project/resources/views/front/checkout.blade.php
@@ -366,14 +366,10 @@
 @endphp
 <script>
    let productArr = <?php echo json_encode($products); ?>;
-   console.log(productArr);
-   var items = [{
-      id: productArr.item
-   }];
-   console.log(items);
-</script>
-<script>
-
+   // console.log(productArr);
+   
+   for(const key in productArr){
+      const element = productArr[key];
    gtag("event", "purchase", {
        transaction_id: "T_12345_1",
        value: 25.42,
@@ -381,29 +377,16 @@
        currency: "USD",
        coupon: {!!$coupon!!},
        items: [
-       // If someone purchases more than one item, 
-       // you can add those items to the items array
         {
-         item_id: "SKU_12345",
-         item_name: "Stan and Friends Tee",
-         affiliation: "Google Merchandise Store",
-         coupon: "SUMMER_FUN",
-         discount: 2.22,
-         index: 0,
-         item_brand: "Google",
-         item_category: "Apparel",
-         item_category2: "Adult",
-         item_category3: "Shirts",
-         item_category4: "Crew",
-         item_category5: "Short sleeve",
-         item_list_id: "related_products",
-         item_list_name: "Related Products",
-         item_variant: "green",
-         location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
-         price: 9.99,
-         quantity: 1
-       }]
+         id: element.item.id,
+         name: element.item.name,
+         slug: element.item.slug,
+         price: element.item.price,
+         quantity: element.item.qty
+      }]
    });
+}
+// console.log(items);
    </script>
 
 
