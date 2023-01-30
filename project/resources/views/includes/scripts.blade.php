c7e2a619e6fe1e22a7b2c374f9827677265b3b49
Abdullah <abdullahwaseem.4401@gmail.com>
2023-01-30 22:56:44 +0500

    sefs

--- project/resources/views/includes/scripts.blade.php
+++ project/resources/views/includes/scripts.blade.php
@@ -1,19 +1,19 @@
-<script defer src="{{asset('assets/frontend-assets/js/jquery.min.js')}}"></script>
-<script defer src="{{asset('assets/frontend-assets/js/sticky.js')}}"></script>
-<script defer src="{{asset('assets/frontend-assets/js/jquery.plugin.min.js')}}"></script>
-<script defer src="{{asset('assets/frontend-assets/js/imagesloaded.pkgd.min.js')}}"></script>
-<script defer src="{{asset('assets/frontend-assets/js/owl.carousel.min.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/jquery.min.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/sticky.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/jquery.plugin.min.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/imagesloaded.pkgd.min.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/owl.carousel.min.js')}}"></script>
 <!-- <script src="{{asset('assets/frontend-assets/js/jquery.zoom.min.js')}}"></script> -->
 <!-- <script src="{{asset('assets/frontend-assets/js/jquery.countdown.min.js')}}"></script> -->
-<script defer src="{{asset('assets/frontend-assets/js/jquery.magnific-popup.min.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/jquery.magnific-popup.min.js')}}"></script>
 <!-- <script src="{{asset('assets/frontend-assets/js/skrollr.min.js')}}"></script> -->
-<script defer src="{{asset('assets/frontend-assets/js/swiper-bundle.min.js')}}"></script>
-<script defer src="{{asset('assets/frontend-assets/js/jquery.zoom.js')}}"></script>
-<script defer src="{{asset('assets/frontend-assets/js/photoswipe.js')}}"></script>
-<script defer src="{{asset('assets/frontend-assets/js/photoswipe-ui-default.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/swiper-bundle.min.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/jquery.zoom.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/photoswipe.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/photoswipe-ui-default.js')}}"></script>
 
 {{-- <script src="{{ asset('assets/dod/js/toastr.min.js') }}"></script> --}}
 
     <!-- Main JS -->
-<script defer src="{{asset('assets/frontend-assets/js/main.min.js')}}"></script>
-<script defer src="{{asset('assets/frontend-assets/js/custom.js')}}?{{ rand(1111,9999) }}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/main.min.js')}}"></script>
+<script async defer src="{{asset('assets/frontend-assets/js/custom.js')}}?{{ rand(1111,9999) }}"></script>
